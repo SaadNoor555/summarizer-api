041ae061654647f5e0c34807eb595c4c8dfcfddf
SaadNoor555 <ssaadn.555@gmail.com>
2023-06-20 04:25:44 +0600

    abstractive summarization added (untested)

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from summarizer import Summarizer
+from transformers import pipeline
+import os
 
 app = FastAPI()
-model = Summarizer()
+exstractive_model = Summarizer()
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+abstructive_summarizer = pipeline('summarization')
+
 
 class Data(BaseModel):
     body: str
 
 @app.post("/extractive/", status_code=200)
-async def extractive_summary(data: Data):
-    result = model(data.body, ratio=0.2)
+async def exstractive_summary(data: Data):
+    result = exstractive_model(data.body, ratio=0.2)
     full = ''.join(result)
     return {'summary': full}
+
+
+@app.post("/abstructive/", status_code=200)
+async def abstructive_summary(data: Data):
+    result = abstructive_summarizer(data.body)[0]['summary_text']
+    return {'summary': result}
     
